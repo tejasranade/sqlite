c6de130fbb374b979189952132deebe641f4b89a
tejasranade <tejas.ranade@gmail.com>
2017-06-13 23:00:35 -0400

    updated to write large number of hierarchy records

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ var Stopwatch = require("node-stopwatch").Stopwatch;
 var stopwatch = Stopwatch.create();
 stopwatch.start();
 
-sdk.service(function(err, flex) {
+//sdk.service(function(err, flex) {
 
 	//const flexFunctions = flex.functions;
 
 	db.serialize(function() {
 
-		db.run("CREATE TABLE if not exists user_info (name TEXT, email TEXT, number NUMBER)");
+		db.run("CREATE TABLE if not exists hierarchy (_id TEXT, SalesOrganization TEXT, DistributionChannel TEXT, ConditionType TEXT, MaterialNumber TEXT, ValidityStartDate TEXT, ValidityEndDate TEXT, Price TEXT, Currency TEXT, DeliveryUnit TEXT, UnitQuantity TEXT, UnitOfMeasure TEXT, SAPCustomerNumber TEXT, _acl TEXT, _kmd TEXT)");
 		// var stmt = db.prepare("INSERT INTO user_info VALUES (?)");
 		// for (var i = 0; i < 10; i++) {
 		//     stmt.run("Ipsum " + i);
@@ -20,15 +20,53 @@ sdk.service(function(err, flex) {
 		db.run("BEGIN");
 
 		var values = [
-		    {name: 'demian', email:'demian@gmail.com', number: 1},
-		    {name:'john', email:'john@gmail.com', number: 2}
+		    {
+		        "_id": "10001000100056071483019ZCSP",
+		        "SalesOrganization": "1000",
+		        "DistributionChannel": "10",
+		        "SAPCustomerNumber": "0010005607",
+		        "MaterialNumber": "1483019",
+		        "ConditionType": "ZCSP",
+		        "SalesDivision": "",
+		        "ValidityStartDate": "2015-10-30",
+		        "ValidityEndDate": "2050-12-31",
+		        "Price": "1.25",
+		        "Currency": "USD",
+		        "DeliveryUnit": "1.000",
+		        "UnitQuantity": "1",
+		        "UnitOfMeasure": "EA",
+		        "_acl": {
+		            "creator": "kid_B15Lb5Pl-"
+		        },
+		        "_kmd": {
+		            "lmt": "2017-05-15T21:50:13.815Z",
+		            "ect": "2017-05-15T21:50:13.815Z"
+		        }
+		    }
 		];
-		var stmt = db.prepare("INSERT INTO user_info VALUES (?, ?, ?)");
+
+		var stmt = db.prepare("INSERT INTO hierarchy VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 		
 		db.serialize(function(){
-			for (var i=0; i<500000; i++){
+			for (var i=0; i<100000; i++){
 				//values.forEach((value) => {
-				stmt.run([values[0].name, values[0].email, values[0].number]);
+				stmt.run([
+					values[0]._id,
+					values[0].SalesOrganization, 
+					values[0].DistributionChannel, 
+					values[0].ConditionType,
+					values[0].MaterialNumber,
+					values[0].ValidityStartDate,
+					values[0].ValidityEndDate,
+					values[0].Price,
+					values[0].Currency,
+					values[0].DeliveryUnit,
+					values[0].UnitQuantity,
+					values[0].UnitOfMeasure,
+					values[0].SAPCustomerNumber,
+					JSON.stringify(values[0]._acl),
+					JSON.stringify(values[0]._kmd)
+					]);
 				//});		
 			}
 			// for (var i=0; i<values.length; i++){
@@ -54,5 +92,4 @@ sdk.service(function(err, flex) {
 
 	console.log("done");
 
-};
-
+//});
