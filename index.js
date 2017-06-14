da53bdc5da10ddd4e11eb7cdf5bffc92f2f31b13
tejasranade <tejas.ranade@gmail.com>
2017-06-14 13:30:02 -0400

    dynamic port

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 })
 
 app.get('/create', function (req, res) {
