652306313f1c8c94e2836badc5d74a251a2402c6
tejasranade <tejas.ranade@gmail.com>
2017-06-14 23:26:41 -0400

    reorg code and added network fetching

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //const express = require('express')
 //const app = express()
+const queue = require('async/queue');
 
 var sqlite3 = require('sqlite3');
 const sdk = require('kinvey-flex-sdk');
@@ -26,89 +27,135 @@ sdk.service((err, flex) => {
 
 	function doStuff(context, complete, modules){
 		console.log("Hit create route...");
-		var stopwatch = Stopwatch.create();
-		stopwatch.start();
 
-	  	db.serialize(function() {
+		preProcess();
 
+		const store = modules.dataStore();
+  		const hierarchy = store.collection('hierarchycache');
+
+  		//var customers = ["SBOOK", ];
+  		var customers = [
+  		//{name: "SBOOK", count:210893}, 
+  		//{name: "MA00056", count: 2261}, 
+  		{name: "MA00452", count: 43825}];
+  		// {name: "MA20313", count: 340}, 
+  		// {name: "MA00040", count: 45131}, 
+  		// {name: "MA00405", count: 49128}, 
+  		// {name: "MA09200", count: 41448}, 
+  		// {name: "MA09208", count: 41688},
+  		// {name: "MA20128", count: 46068},
+  		// {name: "MA20404", count: 201670},
+  		//{name: "MA20280", count: 202011}];
+  		console.log(customers);
+
+  		var resultSet = [];
+
+  		var q = queue(function (query, callback) {
+  			hierarchy.find(query, callback);
+  		}, 25);
+
+  		q.drain = function (){
+  			console.log("fetched everything -");
+  			console.log(resultSet.length);
+  			postProcess();
+
+  			const cacheReady = modules.dataStore().collection('cacheReady');
+  			const user = modules.requestContext.getAuthenticatedUserId();
+		    cacheReady.save({userId: user}, (err, savedResult) => {
+		      if (err) {
+		        //return complete().setBody(err).runtimeError().done();
+		        console.log("error saving status");
+		        return;
+		      }
+		      //complete().setBody(savedResult).ok().next();
+		      console.log("success saving status");
+		    });
+
+  			//return complete().setBody("complete").ok().done();
+  		};
+
+  		customers.map(function (customer){
+
+  		  	var skipCount = 0;
+  		  	do {
+  		  		const query = new modules.Query();
+  		  		query.equalTo('SAPCustomerNumber', customer.name);
+  		  		query.ascending('_id');
+  		  		query.limit = 10000;
+  		  		query.skip = skipCount;
+  		  		
+  		  		console.log("query: " + query);
+	  		  	q.push(query, (err, values) => {
+	  				//console.log(values);
+	  				console.log("fetched page:" + values.length);
+	  				persist(values);
+	  				//resultSet = resultSet.concat(values);
+	  			});	
+  		  		skipCount += 10000;
+  		  	} while (skipCount < customer.count); 
+  		});
+
+  		return complete().setBody("request accepted").ok().done();
+
+  // 		for(var i=0; i<5; i++){
+		// }
+
+  // 		//parallel([], (err, results) => {
+  // 		hierarchy.find(query, (err, values) => {
+  // 			console.log("error - " + err);
+  // 			console.log("results length - " + values.length);
+  // 			console.log("results - " + values);
+
+
+  // 			});
+		// });
+	}
+
+	function preProcess(){
+		db.serialize(function() {
 			db.run("CREATE TABLE if not exists hierarchy (_id TEXT, SalesOrganization TEXT, DistributionChannel TEXT, ConditionType TEXT, MaterialNumber TEXT, ValidityStartDate TEXT, ValidityEndDate TEXT, Price TEXT, Currency TEXT, DeliveryUnit TEXT, UnitQuantity TEXT, UnitOfMeasure TEXT, SAPCustomerNumber TEXT, _acl TEXT, _kmd TEXT)");
 			db.run("PRAGMA synchronous = OFF");
 			db.run("PRAGMA journal_mode = MEMORY");
-			// var stmt = db.prepare("INSERT INTO user_info VALUES (?)");
-			// for (var i = 0; i < 10; i++) {
-			//     stmt.run("Ipsum " + i);
-			// }
+		});
+	}
+
+	function persist(values){
+		db.serialize(function() {
+
 			db.run("BEGIN");
+			var stmt = db.prepare("INSERT INTO hierarchy VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+			console.log("starting insert");
 
-			var values = [
-	        {
-	            "_id": "10001000100056071483019ZCSP",
-	            "SalesOrganization": "1000",
-	            "DistributionChannel": "10",
-	            "SAPCustomerNumber": "0010005607",
-	            "MaterialNumber": "1483019",
-	            "ConditionType": "ZCSP",
-	            "SalesDivision": "",
-	            "ValidityStartDate": "2015-10-30",
-	            "ValidityEndDate": "2050-12-31",
-	            "Price": "1.25",
-	            "Currency": "USD",
-	            "DeliveryUnit": "1.000",
-	            "UnitQuantity": "1",
-	            "UnitOfMeasure": "EA",
-	            "_acl": {
-	                "creator": "kid_B15Lb5Pl-"
-	            },
-	            "_kmd": {
-	                "lmt": "2017-05-15T21:50:13.815Z",
-	                "ect": "2017-05-15T21:50:13.815Z"
-	            }
-	        }
-	    ];
-
-	    var stmt = db.prepare("INSERT INTO hierarchy VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-			
 			db.serialize(function(){
-				for (var i=0; i<10; i++){
+				for (var i=0; i<values.length; i++){
 					//values.forEach((value) => {
 					stmt.run([
-						values[0]._id,
-						values[0].SalesOrganization, 
-						values[0].DistributionChannel, 
-						values[0].ConditionType,
-						values[0].MaterialNumber,
-						values[0].ValidityStartDate,
-						values[0].ValidityEndDate,
-						values[0].Price,
-						values[0].Currency,
-						values[0].DeliveryUnit,
-						values[0].UnitQuantity,
-						values[0].UnitOfMeasure,
-						values[0].SAPCustomerNumber,
-						JSON.stringify(values[0]._acl),
-						JSON.stringify(values[0]._kmd)
-						]);
-					//});		
+						values[i]._id,
+						values[i].SalesOrganization, 
+						values[i].DistributionChannel, 
+						values[i].ConditionType,
+						values[i].MaterialNumber,
+						values[i].ValidityStartDate,
+						values[i].ValidityEndDate,
+						values[i].Price,
+						values[i].Currency,
+						values[i].DeliveryUnit,
+						values[i].UnitQuantity,
+						values[i].UnitOfMeasure,
+						values[i].SAPCustomerNumber,
+						JSON.stringify(values[i]._acl),
+						JSON.stringify(values[i]._kmd)
+					]);
 				}
-				// for (var i=0; i<values.length; i++){
-				// 	stmt.run(values[i]);
-				// }
-				//stmt.run();
-	      stmt.finalize();
+
+		  		stmt.finalize();
 			});
-
-			db.run("COMMIT");
-
-			// db.each("SELECT rowid AS id, info FROM user_info", function(err, row) {
-			// 	//console.log(row.id + ": " + row.info);
-			// });
-
-			console.log("transaction done");
+		      	
+		    db.run("COMMIT");
 		});
+	}
 
-
-		stopwatch.stop();
-
+	function postProcess(){
 		db.close(function (error, result) {
 			if (error) {
 				console.log("DB close error: " + close);
@@ -130,14 +177,14 @@ sdk.service((err, flex) => {
 			Kinvey.initialize({
 			  apiHostname: 'https://v3yk1n-kcs.kinvey.com',
 			  appKey: 'kid_B15Lb5Pl-',
-			  appSecret: 'b7b92c030f2d446999d66ad2f3c50c88'
+			  appSecret: ''
 			}).then(function(activeUser) {
 				// var promise = Kinvey.ping().then(function(response) {
 				//   console.log('Kinvey Ping Success. Kinvey Service is alive, version: ' + response.version + ', response: ' + response.kinvey);
 				// }).catch(function(error) {
 				//   console.log('Kinvey Ping Failed. Response: ' + error.description);
 				// });
-				var promise = Kinvey.User.login('ccalato', 'ccalato').then(function onSuccess(user) {
+				var promise = Kinvey.User.login('ccalato', '').then(function onSuccess(user) {
 					console.log("User logged in...");
 					var fileContent = fs.readFile('./kinveyOffline.sqlite.gz', function (err,fileContent) {
 						if (err) {
@@ -155,8 +202,7 @@ sdk.service((err, flex) => {
 						var promise = Kinvey.Files.upload(fileContent, metadata)
 						  .then(function(file) {
 						  	console.log("File upload succeeded...");
-						  	console.log(file);						  	
-						  	return complete().setBody("Kinvey complete").ok().done();
+						  	console.log(file);						  							  	
 						  })
 						  .catch(function(error) {
 						  	console.log("File upload error.");
@@ -173,12 +219,9 @@ sdk.service((err, flex) => {
 			})
 
 		});
-		//console.log("done");
-		//res.send("seconds: " + stopwatch.elapsed.seconds);
 	}
 
 	flex.functions.register('seedCache', doStuff);
-
 });
 
 //})
